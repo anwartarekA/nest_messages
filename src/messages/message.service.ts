7917bb863a3ede1e161aa5a250bb86ab8d56f21d
= <=>
2026-03-30 04:17:24 +0100

    reuse repo methods in service

--- src/messages/message.service.ts
+++ src/messages/message.service.ts
@@ -1,2 +1,18 @@
 import { MessageRepository } from './message.repository';
-export class MessageService {}
+export class MessageService {
+  messageRepo: MessageRepository;
+  constructor() {
+    // create it's dependancy on it's own and don't do that
+    const repo = new MessageRepository();
+    this.messageRepo = repo;
+  }
+  findAll() {
+    return this.messageRepo.findAll();
+  }
+  findOne(id) {
+    return this.messageRepo.findOne(id);
+  }
+  create(content) {
+    return this.messageRepo.create(content);
+  }
+}
